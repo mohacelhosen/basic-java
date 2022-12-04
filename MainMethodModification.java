2ae2691a315001cfc5f3dd0de147f11dc418bd4e
Mohacel <118649370+mohacelhosen@users.noreply.github.com>
2022-12-04 19:16:54 +0600

    Add files via upload

---
+++ MainMethodModification.java
@@ -0,0 +1,7 @@
+package Demo;
+
+public class MainMethodModification {
+    static  final strictfp synchronized public void main(String... args){
+        System.out.println("This is the main() method");
+    }
+}
