48c15d2aee37148c16117b2190cd400f582512e8
Mohacel <118649370+mohacelhosen@users.noreply.github.com>
2023-02-13 17:00:10 +0600

    Add files via upload

---
+++ Operators_Doubt.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Operators_Doubt {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        System.out.println(" Integer/Integer result always (10/3) = "+(x/y)+" ( Integer) ");
+
+        int a = 4;
+        int b = 9;
+        System.out.println("If numerator < denominator then reminder always (4/9) = "+(a % b)+" (numerator)");
+    }
+}
